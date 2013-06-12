d97a0e10a7b9cb687d8b775dab6539c2ae75777c
unknown <FChasseur@AMD-GPBP05J.histoscanning.com>
2013-06-12 15:40:38 +0200

    Better client

--- slave/client.py
+++ slave/client.py
@@ -1,27 +1,33 @@
 from socketIO_client import SocketIO
 import os
+import glob 
 
-def on_aaa_response(*args):
-    print 'on_aaa_response', args
+filePattern = '*.py';
 
+def getMovieList():
+	mvs = glob.glob(filePattern);
+	return mvs;
 	
-def on_bbb_response(*args):
-    print 'on_start', args
-	
-	
+def getMovieListResponse():
+	dic = {}
+	dic["id"] ="me";
+	dic["pid"] = os.getpid(); 
+	dic["movieList"] = getMovieList();
+	return dic;
+
+
+
 def on_newPiConnected_response(*args):
     print 'on_newPiConnected_response', args
 	
 def on_getMovieList(*args):
     global socketIO
     pid = os.getpid();
-    socketIO.emit('onMovieListResponse', {'id': 'me','movieList': ['m1','m2'], 'pid' : pid } );	
+    socketIO.emit('onMovieListResponse', getMovieListResponse() );	
 
-socketIO = SocketIO('192.168.2.84', 8082)
-socketIO.on('aaa_response', on_aaa_response)
-socketIO.on('start', on_bbb_response)
+socketIO = SocketIO('192.168.2.166', 8082)
 socketIO.on('newPiConnected', on_newPiConnected_response)
 socketIO.on('getMovieList',on_getMovieList);
-
+print(getMovieList());
 socketIO.emit('newPiConnected')
 socketIO.wait()
