5a1aaedfd35581824a98483f1e75586fafb7ab34
digicious <cha@digicious.be>
2013-07-29 10:38:41 +0200

    Adding setup Script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+apt-get install git
+cd ~
+git clone https://github.com/digicious/omxplayer-sync.git
+git clone  https://github.com/digicious/omxplayer.git
+git clone https://github.com/digicious/rpiVideoSync.git
+
